002329e79787c3f642b249c6777416fd8a9e6fe5
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2025-07-20 00:46:51 +1200

    Commit 4

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    color: {
+      DEVS: "#795ddd",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
