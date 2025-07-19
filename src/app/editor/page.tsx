436f15381c121caa706428dfebf166dcc0a5ce91
834N5 <pkgduk@disroot.org>
2025-07-19 14:53:25 +1200

    create editor

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { useState } from 'react';
+import { IText } from 'fabric'
+
+import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+
+export default function Home() {
+  const { editor, onReady } = useFabricJSEditor();
+  const [canvasState, setCanvasState] = useState([])
+  const onAddCircle = () => {
+    editor?.addCircle();
+  };
+  const onAddRectangle = () => {
+    editor?.addRectangle();
+  };
+  const onAddText = () => {
+    const tb = new IText('foobar');
+    editor?.canvas.add(tb);
+  };
+  const onDelete = () => {
+    editor?.deleteSelected();
+  };
+  const onSetFillColor = () => {
+    editor?.setFillColor("#FF0000");
+  };
+
+  const onExport = () => {
+    setCanvasState(editor?.canvas.toJSON());
+  };
+  const onImport = () => {
+    editor?.canvas.loadFromJSON(canvasState).then(
+      (canvas) => canvas.requestRenderAll()
+    );
+  };
+  const onDebug = () => {
+    console.log(canvasState);
+  };
+
+  return (
+    <div>
+      <button onClick={onAddCircle}>Add circle</button>
+      <button onClick={onAddRectangle}>Add Rectangle</button>
+      <button onClick={onAddText}>Add Text</button>
+      <button onClick={onSetFillColor}>Make Red</button>
+      <button onClick={onDelete}>Delete</button>
+      <button onClick={onExport}>Export</button>
+      <button onClick={onImport}>Import</button>
+      <button onClick={onDebug}>Debug</button>
+      <FabricJSCanvas className="sample-canvas" onReady={onReady} />
+    </div>
+  );
+}
