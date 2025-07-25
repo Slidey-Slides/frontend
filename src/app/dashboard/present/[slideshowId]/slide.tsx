023561f6944b9abdccefa9036d3dc07dbbe50206
FusionSid <siddheshadsv@icloud.com>
2025-07-25 23:36:48 +1200

    feat: randomly generate room code and full screen button

--- src/app/dashboard/present/[slideshowId]/slide.tsx
+++ src/app/dashboard/present/[slideshowId]/slide.tsx
@@ -1,20 +1,26 @@
 "use client";
-import { useEffect, useCallback } from "react";
+
 import * as fabric from "fabric";
+import { useEffect, useCallback, useRef } from "react";
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 import { usePresentationSocket } from "./hook";
 
 type LoadJSONArg = Parameters<fabric.Canvas["loadFromJSON"]>[0];
 
 const Slide = (props: { slideData: LoadJSONArg[] }) => {
+  const handle = useFullScreenHandle();
+  const randomRoomCode = useRef(
+    Math.floor(100000000 + Math.random() * 900000000),
+  );
   const { editor, onReady } = useFabricJSEditor();
 
   const nextSlide = useCallback(() => {}, []);
   const prevSlide = useCallback(() => {}, []);
 
   const { isReady, slideNumber } = usePresentationSocket({
-    code: 123456789,
+    code: randomRoomCode.current,
     slideshowData: JSON.stringify(props.slideData),
 
     onNextSlide: nextSlide,
@@ -30,17 +36,26 @@ const Slide = (props: { slideData: LoadJSONArg[] }) => {
       .then((canvas) => canvas.requestRenderAll());
   }, [editor?.canvas, props.slideData, slideNumber]);
 
+  if (!isReady) {
+    return <>Not yet connected to socket server!</>;
+  }
+
   return (
-    <>
-      {isReady ? (
+    <div>
+      <div className="flex justify-between">
+        <button className="hover:cursor-pointer" onClick={handle.enter}>
+          Enter fullscreen
+        </button>
+        <h1>Room Code: {randomRoomCode.current}</h1>
+      </div>
+
+      <FullScreen handle={handle}>
         <FabricJSCanvas
           className="sample-canvas h-[100vh] border-2 border-[#373937] bg-[#030507]"
           onReady={onReady}
         />
-      ) : (
-        <>NOT READY</>
-      )}
-    </>
+      </FullScreen>
+    </div>
   );
 };
 
